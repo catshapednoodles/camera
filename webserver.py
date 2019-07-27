59675418a5f51eb54b0d5159b5fa86d22253921b
Sicco de Haan <sicco92@gmail.com>
2019-07-27 22:51:35 +0200

    flask template added

--- webserver.py
+++ webserver.py
@@ -1,3 +1,4 @@
+import os.path
 from flask import Flask, render_template, send_file
 
 HOST = "0.0.0.0"             # host ip adress do not change
@@ -5,15 +6,34 @@ PORT = 8080                  # only for debug and test environment. will change 
 DebugMode = True             # set to false if server is deployed
 server = Flask(__name__)     # main server variable
 
+def make_tree(path):
+    tree = dict(name=os.path.basename(path), children=[])
+    try: lst = os.listdir(path)
+    except OSError:
+        pass #ignore errors
+    else:
+        for name in lst:
+            fn = os.path.join(path, name)
+            if os.path.isdir(fn):
+                tree['children'].append(make_tree(fn))
+            else:
+                tree['children'].append(dict(name=name))
+    return tree
+
+#@server.route("/")
+#def Home():
+#    return render_template("home.html")
+
 @server.route("/")
-def Home():
-    return render_template("home.html")
+def dirtree():
+    path = os.path.expanduser(u'~')
+    return render_template('dirtree.html', tree=make_tree(path))
 
 
-@server.route("/<image>.jpg")
-def Image(image=""):
+@server.route("/randomhere")
+def Images():
     path = "../" + image + ".jpg"
     return send_file(path, mimetype="image/jpg")
 
-
-server.run(debug=DebugMode,host=HOST,port=PORT)
+if __name__=="__main__":
+    server.run(debug=DebugMode,host=HOST,port=PORT)
