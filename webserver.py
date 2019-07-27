3caa26ca3f7b171668ad5b2a203f72db77707ef3
Sicco de Haan <sicco92@gmail.com>
2019-07-27 22:13:21 +0200

    flask template added

--- webserver.py
+++ webserver.py
@@ -1,2 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
 
+HOST = "0.0.0.0"             # host ip adress do not change
+PORT = 8080                  # only for debug and test environment. will change to 80 if product is released
+DebugMode = True             # set to false if server is deployed
+server = Flask(__name__)     # main server variable
+
+@server.route("/")
+def Home():
+    return render_template("home.html")
+
+
+@server.route("/<image>.jpg")
+def Image():
+    path = "../" + image + ".jpg"
+    return send_file(path, mimetype="image/jpg")
